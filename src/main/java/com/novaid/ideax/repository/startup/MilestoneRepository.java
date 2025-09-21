abc9ed94320d610a3b214e9c069230056c888322
Tran Dang Minh Quan <minbh.quan648@gmail.com>
2025-09-22 02:16:00 +0700

    Quan update startup feature

---
+++ src/main/java/com/novaid/ideax/repository/startup/MilestoneRepository.java
@@ -0,0 +1,13 @@
+package com.novaid.ideax.repository.startup;
+
+
+import com.novaid.ideax.entity.startup.Milestone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MilestoneRepository extends JpaRepository<Milestone, Long> {
+    List<Milestone> findByProjectId(Long projectId);
+}
